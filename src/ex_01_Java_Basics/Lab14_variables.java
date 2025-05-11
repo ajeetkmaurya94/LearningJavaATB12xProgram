cbb9fb8ce64bdafb2c7fe85fd63e9582f57dda70
Ajeet <mauryaajeet008.com>
2025-05-11 10:06:16 +0530

    commit it

---
+++ src/ex_01_Java_Basics/Lab14_variables.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab14_variables {
+
+
+    public static void main(String[] args) {
+
+        byte age = 65;
+        age=66;
+        System.out.println(age);
+
+    }
+}

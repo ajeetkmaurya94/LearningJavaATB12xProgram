6c326530d4b0e5330786e3068ea5e9910f7ada4f
Ajeet <mauryaajeet008.com>
2025-05-11 10:47:04 +0530

    commit it

---
+++ src/ex_01_Java_Basics/Lab016.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab016 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        //System.out.println(a);
+       // System.out.printf("Your variable is %d" , a);
+
+        int b=20;
+        System.out.printf("%d , %d" , a, b);
+    }
+}
